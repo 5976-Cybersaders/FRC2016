0cf81262ad0b58d11737216f4c9e54374ae29f26
Ryan Knuese <ryanknuese@gmail.com>
2016-02-06 12:08:10 -0600

    Update with code

--- src/org/usfirst/frc/team5976/robot/Robot.java
+++ src/org/usfirst/frc/team5976/robot/Robot.java
@@ -1,8 +1,7 @@
 
 package org.usfirst.frc.team5976.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -18,6 +17,15 @@ public class Robot extends IterativeRobot {
     final String customAuto = "My Auto";
     String autoSelected;
     SendableChooser chooser;
+	RobotDrive robotDrive;  // class that handles basic drive operations
+    XBoxController xBox1, xBox2;
+    PowerDistributionPanel pdp;
+    Compressor compressor;
+    DoubleSolenoid solenoid;
+    
+    public Robot() {
+        
+    }
 	
     /**
      * This function is run when the robot is first started up and should be
@@ -28,6 +36,15 @@ public class Robot extends IterativeRobot {
         chooser.addDefault("Default Auto", defaultAuto);
         chooser.addObject("My Auto", customAuto);
         SmartDashboard.putData("Auto choices", chooser);
+        //Our stuff
+        robotDrive = new RobotDrive(0, 1);
+        robotDrive.setExpiration(0.1);
+        xBox1 = new XBoxController(0);
+        xBox2 = new XBoxController(1);
+        pdp = new PowerDistributionPanel();
+        compressor = new Compressor();
+        compressor.setClosedLoopControl(true);
+        solenoid = new DoubleSolenoid(0, 1);
     }
     
 	/**
@@ -50,21 +67,63 @@ public class Robot extends IterativeRobot {
      */
     public void autonomousPeriodic() {
     	switch(autoSelected) {
-    	case customAuto:
-        //Put custom auto code here   
-            break;
-    	case defaultAuto:
-    	default:
-    	//Put default auto code here
-            break;
+    		case customAuto:
+    			//Put custom auto code here   
+    			System.out.println("Running custom autonomous...");
+    			break;
+    		case defaultAuto:
+    		default:
+    			//Put default auto code here
+    			System.out.println("Running default autonomous...");
+    			break;
     	}
     }
 
+    public void teleopInit() {
+        System.out.println("Teleoperated init");
+      }
+    
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        
+    	double xBoxRight, xBoxLeft;
+    	boolean enabled = compressor.enabled();
+    	boolean pressureSwitch = compressor.getPressureSwitchValue();
+    	float current = compressor.getCompressorCurrent();
+    	
+    	//System.out.println("Compressor values: " + enabled + " " + pressureSwitch + " " + current);
+    	
+    	while (isOperatorControl() && isEnabled()) {
+        	xBoxRight = xBox1.getRightJoyY();
+        	xBoxLeft = xBox1.getLeftJoyY();
+        	
+        	System.out.println();
+        	
+        	robotDrive.tankDrive(adjustSpeed(xBoxLeft), adjustSpeed(xBoxRight));
+        	
+        	if(xBox2.getButtonLB()){
+        		System.out.println("SOLENOID FORWARD");
+        		solenoid.set(DoubleSolenoid.Value.kForward);
+        	}
+        	else{
+        		if(xBox2.getButtonRB() && solenoid.get() != DoubleSolenoid.Value.kReverse){
+        			System.out.println("SOLENOID REVERSE");
+        			solenoid.set(DoubleSolenoid.Value.kReverse);
+        		}
+        		else{
+        			System.out.println("SOLENOID OFF");
+        			solenoid.set(DoubleSolenoid.Value.kOff);
+        		}
+        	}
+            
+        	Timer.delay(0.005);
+        }
+    }
+    
+    public void disabledInit(){
+    	compressor.setClosedLoopControl(false);
+    	solenoid.set(DoubleSolenoid.Value.kOff);
     }
     
     /**
@@ -74,4 +133,21 @@ public class Robot extends IterativeRobot {
     
     }
     
+    /*public static void main(String [] args){
+    	adjustSpeed(1.0);
+    	adjustSpeed(0.5);
+    	adjustSpeed(0.25);
+    	adjustSpeed(0);
+    	adjustSpeed(-0.25);
+    	adjustSpeed(-0.5);
+    	adjustSpeed(-1.0);
+    }*/
+    
+    static double expoFactor = 0.2;
+    
+    public static double adjustSpeed(double d){
+    	double speed = Math.signum(d) * Math.pow(Math.abs(d), Math.pow(4, expoFactor));
+    	//System.out.println("Input: " + d + " Output: " + speed);
+    	return speed;
+    }
 }
